803bb128c324883a48ab817965647c8df9935515
Alireza <alireza@ghaedamini.de>
2025-05-07 14:05:29 +0200

    pushing Expression.java

---
+++ Expression.java
@@ -0,0 +1,26 @@
+
+import java.util.Stack;
+
+public class Expression {
+
+    public boolean isBalanced(String input) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : input.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else if (ch == ')' || ch == '}' || ch == ']') {
+                if (stack.isEmpty())
+                    return false;
+                char top = stack.pop();
+                if (
+                        ch == ')' && top != '(' ||
+                                ch == '}' && top != '{' ||
+                                ch == ']' && top != '['
+                ) return false;
+
+            }
+        }
+        return stack.isEmpty();
+    }
+
+}
